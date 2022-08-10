4b8fec5dedfa706f6e80f99b7177a1d9fc5f42b4
Mario Miranda <mario_chan15@hotmail.com>
2022-08-10 14:02:52 -0400

    Envoie d'email

--- integrations/src/main/java/com/example/integrations/resources/EmailResource.java
+++ integrations/src/main/java/com/example/integrations/resources/EmailResource.java
@@ -1,5 +1,6 @@
 package com.example.integrations.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,13 +8,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.integrations.dto.EmailDTO;
+import com.example.integrations.services.EmailService;
 
 @RestController
 @RequestMapping(value = "/emails")
 public class EmailResource {
 
+	@Autowired
+	private EmailService service;
+	
 	@PostMapping
 	public ResponseEntity<Void> send(@RequestBody EmailDTO dto) {
+		this.service.sendEmail(dto);
 		return ResponseEntity.noContent().build();
 	}
 }
